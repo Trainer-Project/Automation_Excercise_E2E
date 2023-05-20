284967f9f5d7b15e61f0723f015db233f267d9f9
Trainer-Project <mohammedalamnyc@gmail.com>
2023-05-20 09:48:32 -0400

    Update

--- src/test/java/javacoding/challenges/ListPractices.java
+++ src/test/java/javacoding/challenges/ListPractices.java
@@ -18,24 +18,8 @@ public class ListPractices {
 	// Collectors -> collectors is final class that extend the object class,Help
 	// of collector we can remove the duplicate value & many more
 
-	// What is the List?
-	// List is an interface
-	// List implements many class like (ArrayList, LinkedList, Vector & stack)
 
-	// ArrayList advantage
-	// ArrayList support dynamic arrays that can grow as needed
-	// ArrayList allows duplicate elements and null values as well
-	// ArrayList class maintain the insertion order
-	// ArrayList is non-synchronized, That means it's faster
-	// ArrayList flowing the index theory java
-	// ArrayList when add the new values increases the size 50%
-	// ArrayList faster then vector
-	// ArrayList slower then array
-	
-	// Array length is fixed
-	// Array is an object as well non-primitive data type
-	// Array is faster 
-	// Single diminution & multi-diminution array
+
 	
 	
 
