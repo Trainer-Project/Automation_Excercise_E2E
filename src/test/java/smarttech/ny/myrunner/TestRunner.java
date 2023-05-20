75215e56973a9f74f8f3e32526ed793134f6050c
Trainer-Project <mohammedalamnyc@gmail.com>
2023-05-19 20:17:10 -0400

    Update pom.xml

--- src/test/java/smarttech/ny/myrunner/TestRunner.java
+++ src/test/java/smarttech/ny/myrunner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 // How do configure testNg with Cucumber?
 // How to run runner class? 
 @CucumberOptions(
-		// plugin is nothing but generate the report
+		// plug in is nothing but generate the report
 		plugin= {"pretty","json:target/cucumber.json" }, 
 		features = { ".//Features/" }, // feature is nothing but location of features file
 		glue = { "smarttech.ny.stepdefinations", "smarttech.ny.hooks" } // glue is nothing but location of stepdefination
