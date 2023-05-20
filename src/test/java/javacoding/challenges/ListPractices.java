a68eeffcb87a026038be47f6d2497da86a786de4
Trainer-Project <mohammedalamnyc@gmail.com>
2023-05-20 09:47:25 -0400

    Update

--- src/test/java/javacoding/challenges/ListPractices.java
+++ src/test/java/javacoding/challenges/ListPractices.java
@@ -2,4 +2,41 @@ package javacoding.challenges;
 
 public class ListPractices {
 
+	// What is the collection?
+	// Ans:- The collection in java a framework that an architecture to store and
+	// manipulate the group of object
+
+	// Differences between collection, Collections & Collectors
+	// Collection -> The Collection is an interface to store and manipulate the
+	// group of the object
+
+	// Collections -> Collections is a utility class in java, Help with with
+	// collections we can
+	// reverse the string/number of value, find out max, min value as well as many
+	// more operations
+
+	// Collectors -> collectors is final class that extend the object class,Help
+	// of collector we can remove the duplicate value & many more
+
+	// What is the List?
+	// List is an interface
+	// List implements many class like (ArrayList, LinkedList, Vector & stack)
+
+	// ArrayList advantage
+	// ArrayList support dynamic arrays that can grow as needed
+	// ArrayList allows duplicate elements and null values as well
+	// ArrayList class maintain the insertion order
+	// ArrayList is non-synchronized, That means it's faster
+	// ArrayList flowing the index theory java
+	// ArrayList when add the new values increases the size 50%
+	// ArrayList faster then vector
+	// ArrayList slower then array
+	
+	// Array length is fixed
+	// Array is an object as well non-primitive data type
+	// Array is faster 
+	// Single diminution & multi-diminution array
+	
+	
+
 }
